3497386c62ac65e01f7e2f8c5775b05a2eb6938f
ivanna <ivannamyckhal@uni-koblenz.de>
2018-12-23 16:49:20 +0100

    specify model path

--- test_policy.py
+++ test_policy.py
@@ -2,6 +2,7 @@ import gym
 import numpy as np
 import tensorflow as tf
 import argparse
+import gym_fetch_base_motions
 from network_models.policy_net import Policy_net
 
 
@@ -9,15 +10,16 @@ def argparser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--modeldir', help='directory of model', default='trained_models')
     parser.add_argument('--alg', help='chose algorithm one of gail, ppo, bc', default='gail')
-    parser.add_argument('--model', help='number of model to test. model.ckpt-number', default='')
+    parser.add_argument('--model', help='model name', default='')
     parser.add_argument('--logdir', help='log directory', default='log/test')
     parser.add_argument('--iteration', default=int(1e3))
     parser.add_argument('--stochastic', action='store_false')
+    parser.add_argument('--env', help='openai enviroment', default='FetchBase-v0')
     return parser.parse_args()
 
 
 def main(args):
-    env = gym.make('CartPole-v0')
+    env = gym.make(args.env)
     env.seed(0)
     Policy = Policy_net('policy', env)
     saver = tf.train.Saver()
@@ -28,7 +30,7 @@ def main(args):
         if args.model == '':
             saver.restore(sess, args.modeldir+'/'+args.alg+'/'+'model.ckpt')
         else:
-            saver.restore(sess, args.modeldir+'/'+args.alg+'/'+'model.ckpt-'+args.model)
+            saver.restore(sess, args.modeldir+'/'+args.alg+'/'+ args.model + '.ckpt')
         obs = env.reset()
         reward = 0
         success_num = 0
@@ -37,16 +39,18 @@ def main(args):
             rewards = []
             run_policy_steps = 0
             while True:  # run policy RUN_POLICY_STEPS which is much less than episode length
+                env.render()
                 run_policy_steps += 1
                 obs = np.stack([obs]).astype(dtype=np.float32)  # prepare to feed placeholder Policy.obs
                 act, _ = Policy.act(obs=obs, stochastic=args.stochastic)
 
-                act = np.asscalar(act)
+               # act = np.asscalar(act)
+                print('Action from model=', act)
 
                 rewards.append(reward)
-
+                
                 next_obs, reward, done, info = env.step(act)
-
+                #done = env._success(None, None)
                 if done:
                     obs = env.reset()
                     reward = -1
